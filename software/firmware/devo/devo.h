5ff984c264bb933f49a80d6f4723356c8cf522b5
xiaonong <nongxiaoming@gmail.com>
2014-12-28 22:56:34 +0800

    fix the error.

--- software/firmware/devo/devo.h
+++ software/firmware/devo/devo.h
@@ -1,5 +1,6 @@
 #ifndef _DEVO_H_
 #define _DEVO_H_
+#include <stdint.h>
 
 #ifndef HAS_STANDARD_GUI
     #define HAS_STANDARD_GUI 1
@@ -8,7 +9,7 @@
     #define HAS_ADVANCED_GUI 1
 #endif
 
-void ADC_Init(void);
+void ADC_HW_Init(void);
 void ADC_StartCapture(void);
 
 enum {
@@ -16,13 +17,13 @@ enum {
     NUM_MSEC_CALLBACKS,
 };
 void CLOCK_ClearMsecCallback(int MsecCallback);
-u32 SOUND_Callback(void);
+uint32_t SOUND_Callback(void);
 
 extern void PROTO_Stubs(int);
 // ADC defines
 #define NUM_ADC_CHANNELS (INP_HAS_CALIBRATION + 2) //Inputs + Temprature + Voltage
-extern const u8 adc_chan_sel[NUM_ADC_CHANNELS];
-extern volatile u16 adc_array_raw[NUM_ADC_CHANNELS];
+extern const uint8_t adc_chan_sel[NUM_ADC_CHANNELS];
+extern volatile uint16_t adc_array_raw[NUM_ADC_CHANNELS];
 void ADC_Filter();
 
 #endif
